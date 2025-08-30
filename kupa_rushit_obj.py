0f16f356ad059bd2a93639e5eae04d08e04dd2d0
Gabriel <Khvalevsky.gabriel@gmail.com>
2025-08-30 17:58:47 +0300

    skeleton-obj

---
+++ kupa_rushit_obj.py
@@ -0,0 +1,35 @@
+class person:
+    def __init__(self):
+        pass
+
+# workers
+class worker(person):
+    pass
+
+class manerger(worker):
+    pass
+
+class cashier(worker):
+    pass
+
+class butcher(worker):
+    pass
+
+class delivery_guy(worker):
+    pass
+
+class general_worker(worker):
+    pass
+
+class securiy_guy(worker):
+    pass
+
+# customers
+class cursomer(person):
+    pass
+
+class repeat_customer(customer):
+    pass
+
+class one_time_customer(customers):
+    pass
