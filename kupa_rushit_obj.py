5642191c56333df7e75601aaf96486f1185f22db
Gabriel <Khvalevsky.gabriel@gmail.com>
2025-09-05 20:22:16 +0300

    Adds documentation to person and customer classes
    Adds docstrings to several methods in the person and customer classes to clarify their intended functionality. This improves code readability and maintainability by documenting the purpose of methods related to vacation scheduling, market entry detection, and customer purchase logging.

--- kupa_rushit_obj.py
+++ kupa_rushit_obj.py
@@ -2,14 +2,28 @@ class person:
     def __init__(self):
         pass
     def vacation(self):
+        """
+        vacation will get list of dates and in these dates there will be no data
+        """
         pass
-
+    def one_day_before_vacation(self):
+        """
+        there will be more workers and more customers
+        """
+        pass
+    def entering_market(self):
+        """
+        because we work with geolocated data we want to let the analyst to know when the object come into the supermarket
+        for that we create the parking lot data - almost every customer will get detected in the parking lot before it entering the supermarket
+        note that will be customers that will not detect in the parking lot for making a bit noise for the students
+        """
+        pass
 # workers
 class worker(person):
     pass
 
 class manerger(worker):
-    pass
+    pass # comes from 8 to 5 and not moving much
 
 class cashier(worker):
     pass
@@ -29,6 +43,10 @@ class securiy_guy(worker):
 # customers
 class cursomer(person):
     pass
+    def purchase(self):
+        """
+        function to insert log of customer bill
+        """
 
 class repeat_customer(customer):
     pass
